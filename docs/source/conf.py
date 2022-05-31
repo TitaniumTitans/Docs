8329262d9c99f0b5ce9a997fedeb41ac3628188b
GearFox <favoringbasil55@gmail.com>
2022-05-31 12:04:04 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Titan Docs'
+copyright = '2022, Titanium Titans'
+author = 'MazeOfEncryption, GearBoxFox'
 
 release = '0.1'
 version = '0.1.0'
